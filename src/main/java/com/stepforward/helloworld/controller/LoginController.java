e588fa858f3d3ad34b98446873225f6225b7efaf
harneet-kaur <harneet2800@gmail.com>
2017-03-10 17:34:08 +0530

    Update LoginController.java

--- src/main/java/com/stepforward/helloworld/controller/LoginController.java
+++ src/main/java/com/stepforward/helloworld/controller/LoginController.java
@@ -4,14 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/authenticate-user", method=RequestMethod.GET)
+	@RequestMapping(value="/authenticateUser", method=RequestMethod.GET)
 	public String AuthenticateUser(@RequestParam("username") String username,
 			@RequestParam("password") String password){
-		System.out.println("HARNEET");
-		return "ok";
+		ModelAndView mv = new ModelAndView("login");
+		
+		if((username !=null && username.length()>0)  && (password !=null && password.length()>0)){
+			if(username.equals("admin") && password.equals("admin")){
+				mv.addObject("response", "YOU WILL BE MOVED ON LANDING PAGE SHORTLY");
+			}else{
+				mv.addObject("response", "INVALID CREDENTIALS");
+			}	
+		}else{
+			mv.addObject("response", "ENTER USERNAME AND PASSWORD");
+		}
+		
+		return password;
+		
 	}
 }
